0712612197063e58130002b971219df89666b95e
aruyamam <aruyamam@gmail.com>
2019-02-07 17:05:27 +0900

    Redesigning the home page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer fixed>
+      <v-list dense>
+        <router-link v-bind:to="{name: 'Home'}" class="side_bar_link">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>Home</v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+        <router-link v-bind:to="{name: 'Contact'}" class="side_bar_link">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>Contact</v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Home</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <div id="app">
+          <router-view/>
+        </div>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2018</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 import "./assets/stylesheets/main.css";
 
 export default {
-  name: "App"
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
+  }
 };
 </script>
 
