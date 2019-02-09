3aa2a622a37d33d37d06d2ce7c49838b74a9a701
aruyamam <aruyamam@gmail.com>
2019-02-09 16:50:55 +0900

    Redesigning the contact page

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Home from './components/Home.vue';
+import Contact from './components/Contact.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
@@ -14,12 +14,9 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/contact',
+      name: 'Contact',
+      component: Contact,
     },
   ],
 });
