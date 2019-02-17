e61d9ff7ba611b541fad63dbe4f24668ce58520e
aruyamam <aruyamam@gmail.com>
2019-02-17 17:45:23 +0900

    Adding form validations

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -2,8 +2,14 @@
   <v-form v-model="valid" ref="form" lazy-validation>
     <v-text-field label="Movie Name*" v-model="name" :rules="nameRules" required></v-text-field>
     <v-text-field name="input-7-1" label="Movie Description" v-model="description" multi-line></v-text-field>
-    <v-select label="Movie Release Year" v-model="release_year" :items="years"></v-select>
-    <v-text-field label="Movie Genre" v-model="genre"></v-text-field>
+    <v-select
+      label="Movie Release Year"
+      v-model="release_year"
+      required
+      :rules="releaseRules"
+      :items="years"
+    ></v-select>
+    <v-text-field label="Movie Genre" v-model="genre" required :rules="genreRules"></v-text-field>
     <v-btn @click="submit" :disabled="!valid">sumbit</v-btn>
     <v-btn @click="clear">clear</v-btn>
   </v-form>
@@ -19,6 +25,13 @@ export default {
     genre: "",
     release_year: "",
     nameRules: [v => !!v || "Movie name is required"],
+    genreRules: [
+      v => !!v || "Movie genre year is required",
+      v =>
+        (v && v.length <= 80) ||
+        "Genre must be less than equal to 80 characters."
+    ],
+    releaseRules: [v => !!v || "Movie release year is required."],
     select: null,
     years: ["2018", "2017", "2016", "2015"]
   }),
